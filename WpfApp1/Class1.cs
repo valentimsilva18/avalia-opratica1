c614a45ba09a604e304acc594255f8a453a29ddd
FranciscoMartins28 <al82084@alunos.utad.pt>
2025-03-12 10:09:39 +0000

    alteraçoes branch

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        public static string GetText()
+        {
+            Francisco Martins 82084
+                   }
+    }
+}
